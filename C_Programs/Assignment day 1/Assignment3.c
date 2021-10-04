ba1da15d70f507ca0a3a62357157630ed48413d1
Nilesh Dhanore <nilesh.sgd@gmail.com>
2021-10-04 22:50:40 +0530

    Push_Assignment3

---
+++ C_Programs/Assignment day 1/Assignment3.c
@@ -0,0 +1,18 @@
+/* This is my First Assignment */
+/* pre processor directive */
+#include<stdio.h>
+
+/* global variable declaration */
+int main()
+{
+
+printf("\n 1. I am Nilesh \n Dhanore" );
+printf("\n 2. I am Nilesh \t Dhanore" );
+printf("\n 3. I am Nilesh \'Dhanore'" );
+printf("\n 4. I am Nilesh \f Dhanore");
+printf("\n 5. I am Nilesh Dhanore \a" );
+
+return 0;
+
+}
+
