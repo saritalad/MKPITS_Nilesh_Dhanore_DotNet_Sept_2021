b18168e3dc327dc3451d4054dfa91462575c79a4
Nilesh Dhanore <nilesh.sgd@gmail.com>
2021-10-04 21:58:14 +0530

    all_Practice_Programms_added_in_github

---
+++ C_Programs/Practice day 1/Second_Program.c
@@ -0,0 +1,16 @@
+/* This is my First Program */
+/* pre processor directive */
+#include<stdio.h>
+/* global variable declaration */
+int main()
+{
+
+char mycell ='r';
+
+printf(" mycell = %c",mycell );
+
+
+return 0;
+
+}
+
