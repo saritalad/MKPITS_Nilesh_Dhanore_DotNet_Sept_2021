445591f22f8c17b9f38c67556bb559f9e19c7cdc
Nilesh Dhanore <nilesh.sgd@gmail.com>
2021-10-04 22:28:31 +0530

    second_assignment

---
+++ C_Programs/Assignment day 1/Assignment2.c
@@ -0,0 +1,38 @@
+/* This is my Second Assignment */
+/* pre processor directive */
+#include<stdio.h>
+/* global variable declaration */
+int main()
+{
+
+char a=18;
+signed char b=14;
+unsigned char c=22;
+short d=34;
+signed short e=32767;
+unsigned short f=65532;
+int g=32732;
+signed int h=2;
+unsigned int i=5;
+long int j=2;
+signed long int k=-4;
+unsigned long int l=5;
+float m=5.8;
+printf("\n a = %d",a);
+printf("\n b = %d",b);
+printf("\n c = %d",c);
+printf("\n d = %d",d);
+printf("\n e = %d",e);
+printf("\n f = %d",f);
+printf("\n g = %d",g);
+printf("\n h = %d",h);
+printf("\n i = %d",i);
+printf("\n j = %d",j);
+printf("\n k = %d",k);
+printf("\n l = %d",l);
+printf("\n m = %0.1f",m);
+
+return 0;
+
+}
+
