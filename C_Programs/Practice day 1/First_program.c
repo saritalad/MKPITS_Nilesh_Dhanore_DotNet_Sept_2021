023fcda33fc300f539603136bab28a6615695320
Nilesh Dhanore <nilesh.sgd@gmail.com>
2021-10-04 21:53:59 +0530

    First_Practice_C_Program

---
+++ C_Programs/Practice day 1/First_program.c
@@ -0,0 +1,16 @@
+/* This is my First Program */
+/* pre processor directive */
+#include<stdio.h>
+
+/* global variable declaration */
+int main()
+{
+
+printf(" Welcome to MKPITs\n ");
+
+printf( "Nilesh \f Dhanore  \a\a\a\a\a");
+return 0;
+
+}
+ 
+
