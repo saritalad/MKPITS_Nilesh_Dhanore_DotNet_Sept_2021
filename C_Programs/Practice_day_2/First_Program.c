aa365e2b4223e057672d538752afae776939d603
Nilesh Dhanore <nilesh.sgd@gmail.com>
2021-10-05 11:14:30 +0530

    all practice file

---
+++ C_Programs/Practice_day_2/First_Program.c
@@ -0,0 +1,20 @@
+/* This is my First Program */
+/* pre processor directive */
+#include<stdio.h>
+
+/* global variable declaration */
+int main()
+{
+int r;
+float pie=3.142,area;
+printf("Enter the value of radius\n");
+scanf("%d",&r);
+area = pie*r*r;
+
+
+printf(" \n area=%0.2f", area );
+
+return 0;
+
+}
+
---
+++ C_Programs/Practice_day_2/Second_Prgm.c
@@ -0,0 +1,20 @@
+/* This is my Second Program */
+/* pre processor directive */
+#include<stdio.h>
+
+/* global variable declaration */
+int main()
+{
+int l,b;
+float area;
+printf("Enter the value of length and breadth\n");
+scanf("%d%d",&l,&b);
+area = l*b;
+
+
+printf(" \n area of rectangle=%0.2f", area );
+
+return 0;
+
+}
+
