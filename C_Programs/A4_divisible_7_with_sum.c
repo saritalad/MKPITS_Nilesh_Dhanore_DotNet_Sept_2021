12584d2dde38d86eba810be3d93860b80c34583f
saritalad <60264225+saritalad@users.noreply.github.com>
2021-10-13 23:07:52 +0530

    Update A4_divisible_7_with_sum.c
    count7 required , sum not required

--- C_Programs/A4_divisible_7_with_sum.c
+++ C_Programs/A4_divisible_7_with_sum.c
@@ -4,17 +4,19 @@
 /* global variable declaration */
 int main()
 {
-  int i, sum=0;
+  int i, sum=0,count7=0;;
   printf("The number divisible by five is\n");
   for(i=1;i<=100;i++)
   {
      if(i%7==0)
-   {
+   { count7++;
     printf("%d\n",i); 
 	sum+=i;
+	     
    }
-   printf("The sum of 
+  
   }
+	 printf("The sum of numbers divisible by 7 =%d\n there are %d numbers divisible by 7",sum,count7);
 return 0;
 }
 
